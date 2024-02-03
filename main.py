7c6dcb430c24297f62e10db7b6554f4bf577663c
iv4n999 <158744825+iv4n999@users.noreply.github.com>
2024-02-03 15:40:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+import os
+import threading
+
+
+os.environ["Version"] = "Stable1.3"
+
+
+# Yandex env
+os.environ["Yandex_Current"] = json.dumps({'type': 'waiting'})
+
+from apps.yandex.main import Yandex
+from apps.discord.main import Discord
+from apps.userinterface.main import run_ui
+yandex_thread = threading.Thread(target=Yandex().run_yandex)
+discord_thread = threading.Thread(target=Discord().run_discord)
+ui_thread = threading.Thread(target=run_ui)
+
+
+def main():
+    yandex_thread.start()
+    discord_thread.start()
+    ui_thread.start()
+
+
+if __name__ == "__main__":
+    main()
